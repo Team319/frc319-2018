f457f16d53cfbbe3391dc369f8738b43f0a8fc27
tytremblay <ty.tremblay@gmail.com>
2018-04-06 10:14:07 -0400

    Remove startHeading from FollowArc, new arcs for testing

--- src/org/usfirst/frc/team319/robot/commands/FollowArc.java
+++ src/org/usfirst/frc/team319/robot/commands/FollowArc.java
@@ -48,15 +48,12 @@ public class FollowArc extends Command {
 		private SrxMotionProfile prof;
 		private final boolean flipped;
 		private double startPosition = 0;
-		private double startHeading = 0;
 
-		public BufferLoader(TalonSRX talon, SrxMotionProfile prof, boolean flipped, double startPosition,
-				double startHeading) {
+		public BufferLoader(TalonSRX talon, SrxMotionProfile prof, boolean flipped, double startPosition) {
 			this.talon = talon;
 			this.prof = prof;
 			this.flipped = flipped;
 			this.startPosition = startPosition;
-			this.startHeading = startHeading;
 		}
 
 		public void run() {
@@ -66,13 +63,13 @@ public class FollowArc extends Command {
 				return;
 			}
 
-			while (!talon.isMotionProfileTopLevelBufferFull() && lastPointSent < prof.numPoints) {
+			if (!talon.isMotionProfileTopLevelBufferFull() && lastPointSent < prof.numPoints) {
 				TrajectoryPoint point = new TrajectoryPoint();
 				/* for each point, fill our structure and pass it to API */
 				point.position = prof.points[lastPointSent][0] + startPosition;
 				point.velocity = prof.points[lastPointSent][1];
 				point.timeDur = TrajectoryDuration.Trajectory_Duration_10ms;
-				point.auxiliaryPos = (flipped ? -1 : 1) * 10 * (prof.points[lastPointSent][3] + startHeading);
+				point.auxiliaryPos = (flipped ? -1 : 1) * 10 * (prof.points[lastPointSent][3]);
 				point.profileSlotSelect0 = distancePidSlot;
 				point.profileSlotSelect1 = rotationPidSlot;
 				point.zeroPos = false;
@@ -116,8 +113,7 @@ public class FollowArc extends Command {
 
 		loadLeftBuffer = new Notifier(
 				new BufferLoader(rightTalon, trajectoryToFollow.centerProfile, trajectoryToFollow.flipped,
-						Robot.drivetrain.getDistance(),
-						Robot.drivetrain.getAngle()));
+						Robot.drivetrain.getDistance()));
 
 		loadLeftBuffer.startPeriodic(.005);
 	}
